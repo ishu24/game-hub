840ba835f0bfd9da6224844a586e78558e8f9c47
ishu24 <voraishani24@gmail.com>
2023-08-26 15:03:59 +0800

    Build game attribute

--- src/Pages/GameDetailPage.tsx
+++ src/Pages/GameDetailPage.tsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
+import DefinitionItem from "../components/DefinitionItem";
+import platforms from "../data/platforms";
+import CriticScore from "../components/CriticScore";
+import GameAttribute from "../components/GameAttribute";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -14,6 +18,7 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttribute game={game}></GameAttribute>
     </>
   );
 };
